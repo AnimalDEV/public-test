d142de59f742455f962b7f40350004e29d03c83f
Maciej Lasoń <eatenanimal@gmail.com>
2020-12-29 23:11:22 +0100

    Add files via upload

--- animal-gh-users.js
+++ animal-gh-users.js
@@ -9,6 +9,8 @@ export default class AnimalGhUsers extends HTMLElement {
         const table = document.createElement('table');
         fetch("https://api.github.com/users").then((res) => res.json()).then((res) => {
             console.log(res);
+            let html = `${res.forEach(({id, login, avatar_url}) => `<tr><td>${id}</td><td>${login}</td><td><img src="${avatar_url}"></td></tr>`)}`;
+            table.insertAdjacentHTML("afterbegin", html);
         });
         this.appendChild(table);
     }
